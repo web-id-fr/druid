ab76d77ab6efd57da89cfe41b686e955eac9a408
CLEMREP <contact@clement-repel.fr>
2023-12-18 10:29:17 +0100

    :lipstick: change icon

--- src/Filament/Resources/MenuResource.php
+++ src/Filament/Resources/MenuResource.php
@@ -17,7 +17,7 @@ class MenuResource extends Resource
 {
     protected static ?string $model = Menu::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-list-bullet';
 
     public static function form(Form $form): Form
     {
