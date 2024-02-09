44910a8fc150562d88516247b42bf74fd68efba5
CLEMREP <contact@clement-repel.fr>
2024-02-09 14:30:48 +0100

    :sparkles: Disable blog with config file

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -9,37 +9,39 @@ use Webid\Druid\App\Http\Controllers\BlogController;
 use Webid\Druid\App\Http\Controllers\FallbackController;
 use Webid\Druid\App\Models\Post;
 
-Route::prefix('{lang}/'.config('cms.blog.prefix'))
-    ->name('posts.multilingual.')
-    ->middleware(['multilingual-required', 'web'])
-    ->group(function () {
-        Route::get('/', [BlogController::class, 'indexMultilingual'])
-            ->name('index');
+if (isBlogDefaultRoutesEnable()) {
+    Route::prefix('{lang}/'.config('cms.blog.prefix'))
+        ->name('posts.multilingual.')
+        ->middleware(['multilingual-required', 'web'])
+        ->group(function () {
+            Route::get('/', [BlogController::class, 'indexMultilingual'])
+                ->name('index');
 
-        Route::get('/categories/{category:slug}', [BlogController::class, 'indexByCategoryMultilingual'])
-            ->name('indexByCategory');
+            Route::get('/categories/{category:slug}', [BlogController::class, 'indexByCategoryMultilingual'])
+                ->name('indexByCategory');
 
-        Route::get('/{post:slug}', [BlogController::class, 'showMultilingual'])
-            ->name('show')
-            ->missing(function (Request $request) {
-                abort(404);
-            });
-    });
+            Route::get('/{post:slug}', [BlogController::class, 'showMultilingual'])
+                ->name('show')
+                ->missing(function (Request $request) {
+                    abort(404);
+                });
+        });
 
-// @phpstan-ignore-next-line
-Route::prefix(config('cms.blog.prefix'))
-    ->name('posts.')
-    ->middleware(['multilingual-forbidden', 'web'])
-    ->group(function () {
-        Route::get('/', [BlogController::class, 'index'])
-            ->name('index');
-        Route::get('/categories/{category:slug}', [BlogController::class, 'indexByCategory'])
-            ->name('indexByCategory');
-        Route::get('/{post:slug}', [BlogController::class, 'show'])
-            ->name('show')
-            ->missing(function (Request $request) {
-                abort(404);
-            });
-    });
+    // @phpstan-ignore-next-line
+    Route::prefix(config('cms.blog.prefix'))
+        ->name('posts.')
+        ->middleware(['multilingual-forbidden', 'web'])
+        ->group(function () {
+            Route::get('/', [BlogController::class, 'index'])
+                ->name('index');
+            Route::get('/categories/{category:slug}', [BlogController::class, 'indexByCategory'])
+                ->name('indexByCategory');
+            Route::get('/{post:slug}', [BlogController::class, 'show'])
+                ->name('show')
+                ->missing(function (Request $request) {
+                    abort(404);
+                });
+        });
+}
 
 Route::fallback([FallbackController::class, 'show'])->middleware('web');
