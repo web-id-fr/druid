8935eb05c51d15810bb260345cb50c4ac2c0e791
LIEGEOIS Elise <75976679+E-Liegeois@users.noreply.github.com>
2024-02-12 17:10:05 +0100

    👷 Fix CI for tests (#29)
    * :wrench: Remove controller and models from publish section
    
    * :art: Use lowercase for migrations directory
    
    * :pencil2: Fix case for migration path in TestServiceProvider
    
    * :white_check_mark: Fix phpstan
    
    * :white_check_mark: Fix stan bis
    
    * :wrench: Update composer
    
    * :fire: Delete unnecessary code
    
    * :white_check_mark: Fix tests
    
    * :bug: Fix factories
    
    * :white_check_mark: Fix phpstan
    
    * :art: Update some stuff
    
    * :bug: Fix factories bis
    
    * :bug: Fix factories bisibis
    
    * :fire: Delete unnecessary code
    
    * :alembic: Try fix factories not found
    
    * :construction_worker: Update CI for tests
    
    * :heavy_plus_sign: Try with legacy-factories
    
    * :heavy_minus_sign: Remove legacy-factories
    
    * :bug: Try fix tests
    
    * :alembic: Try fix factories not found
    
    * :alembic: Try fix factories not found
    
    * :alembic: Try fix factories not found
    
    * :construction_worker: Update CI for tests
    
    * :construction: Fixing Database directory
    
    * :bug: Fix database directory name
    
    * :rewind: Revert unnecessary changement
    
    * :construction: Fixing Database directory
    
    * :bug: Fix database directory name
    
    * :white_check_mark: Fix tests
    
    ---------
    
    Co-authored-by: Benjamin Niess <benjamin@web-id.fr>
    Co-authored-by: Ryan Gilles <ryan@web-id.fr>

--- src/database/factories/MenuItemFactory.php
+++ src/database/factories/MenuItemFactory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Webid\Druid\App\Enums\MenuItemTarget;
 use Webid\Druid\App\Models\Menu;
 use Webid\Druid\App\Models\MenuItem;
+use Webid\Druid\App\Models\Page;
 
 class MenuItemFactory extends Factory
 {
@@ -47,7 +48,7 @@ class MenuItemFactory extends Factory
     public function withPageItem(array $params = []): self
     {
         return $this->state(function () use ($params) {
-            /** @var \Webid\Druid\App\Models\Page $page */
+            /** @var Page $page */
             $page = PageFactory::new()->create($params);
 
             return [
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Webid\Druid\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\View;
@@ -23,11 +22,6 @@ class TestCase extends OrchestraTestCase
     public function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(function (string $modelName) {
-            // Charge les factories du core
-            return 'Webid\\Druid\\Database\\Factories\\'.class_basename($modelName).'Factory';
-        });
 
         $this->loadLaravelMigrations(['--database' => 'mysql']);
         $this->artisan('migrate', ['--database' => 'mysql'])->run();
