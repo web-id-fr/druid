24485342cc6fb5996dfd84decd214d8d5845adbd
E-Liegeois <75976679+E-Liegeois@users.noreply.github.com>
2025-02-05 17:57:59 +0000

    :lipstick: Fix styling

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -3,7 +3,6 @@
 namespace Webid\Druid\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
