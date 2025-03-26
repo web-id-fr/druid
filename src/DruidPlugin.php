211e96a0df616db9d507a2a749b7bdbfa0718401
CLEMREP <contact@clement-repel.fr>
2025-03-26 08:44:46 +0100

    :sparkles: Add DruidPlugin for Filament integration and update README instructions

---
+++ src/DruidPlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Webid\Druid;
+
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+
+class DruidPlugin implements Plugin
+{
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
+    public function getId(): string
+    {
+        return 'druid';
+    }
+
+    public function register(Panel $panel): void
+    {
+        $panel
+            ->discoverResources(
+                in: base_path('vendor/webid/druid/src/Filament/Resources'),
+                for: 'Webid\\Druid\\Filament\\Resources'
+            )
+            ->discoverPages(
+                in: base_path('vendor/webid/druid/src/Filament/Pages'),
+                for: 'Webid\\Druid\\Filament\\Pages'
+            );
+    }
+
+    public function boot(Panel $panel): void
+    {
+        //
+    }
+}
